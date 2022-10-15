7e0c42e119bea3ac845a756f23cff840f028ee43
Federico Carota <fedecarota.fc@gmail.com>
2022-10-15 17:10:35 -0300

    Se verifico la correcta utilizacion de PascalCase y snake_case. Y se hicieron algunas modificaciones acorde al PEP8.

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from home import views
 
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('ver-familiar/', views.ver_personas, name='ver_personas'),
--- home/views.py
+++ home/views.py
@@ -1,50 +1,54 @@
-from django.http import HttpResponse
 from datetime import datetime
-from django.template import Context, Template, loader
 from django.shortcuts import render, redirect
-import random
 from home.models import Persona
 from home.forms import HumanoFormulario, BusquedaHumanoFormulario
 
 
 def crear_persona(request):
-    
     if request.method == 'POST':
-        
         formulario = HumanoFormulario(request.POST)
-        
+
         if formulario.is_valid():
             data = formulario.cleaned_data
-        
+
             nombre = data['nombre']
             apellido = data['apellido']
             edad = data['edad']
             fecha_nacimiento = data.get('fecha_nacimiento', datetime.now())
-            
-            persona = Persona(nombre=nombre, apellido=apellido, edad=edad, fecha_nacimiento=fecha_nacimiento)
+
+            persona = Persona(nombre=nombre,
+                              apellido=apellido,
+                              edad=edad,
+                              fecha_nacimiento=fecha_nacimiento)
             persona.save()
-            
+
             return redirect('ver_personas')
         else:
-            return render(request, 'home/crear_persona.html', {'formulario': formulario})
-         
+            return render(request,
+                          'home/crear_persona.html',
+                          {'formulario': formulario})
+
     formulario = HumanoFormulario()
-    
-    return render(request, 'home/crear_persona.html', {'formulario': formulario})
+
+    return render(request,
+                  'home/crear_persona.html',
+                  {'formulario': formulario})
+
 
 def ver_personas(request):
-    
     nombre = request.GET.get('nombre', None)
-    
+
     if nombre:
         familiares = Persona.objects.filter(nombre__icontains=nombre)
     else:
         familiares = Persona.objects.all()
-    
+
     formulario = BusquedaHumanoFormulario()
-    
-    return render(request, 'home/ver_personas.html', {'familiares': familiares,'formulario': formulario} )
+
+    return render(request,
+                  'home/ver_personas.html',
+                  {'familiares': familiares,'formulario': formulario})
+
 
 def index (request):
-    
     return render(request, 'home/index.html')
